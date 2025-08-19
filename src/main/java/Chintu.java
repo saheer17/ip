48c25c34d3843e40d8b72cbe9db3f6183b373622
saheer17 <saheer1703@gmail.com>
2025-08-19 22:42:02 +0800

    Add greeting messages

--- src/main/java/Chintu.java
+++ src/main/java/Chintu.java
@@ -5,6 +5,10 @@ public class Chintu {
                 + "| |    | |_| || ||  \\| | | | | | | |\n"
                 + "| |___ |  _  || || |\\  | | | | |_| |\n"
                 + " \\____||_| |_|___|_| \\_| |_|  \\___/ \n";
+        String greet_hello = "Hello! I'm Chintu!\n" +
+                "What can I do for you?\n";
+        String greet_bye = "Bye! Take care and hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
+        System.out.println(greet_hello + greet_bye);
     }
 }
