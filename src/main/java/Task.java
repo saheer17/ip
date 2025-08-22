a473a6004abdbc63a878c1786e7bead1b807b778
saheer17 <saheer1703@gmail.com>
2025-08-22 20:26:34 +0800

    Add isDone variable to Task for tracking completion status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,25 @@
 public class Task {
     private String content;
+    private boolean isDone;
 
     public Task(String content) {
         this.content = content;
+        this.isDone = false;
     }
 
-    public String getContent() {
+    public String getTask() {
         return content;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
     }
 }
