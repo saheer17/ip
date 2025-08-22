5af7d0c568f2bfec95f0ad9d1bf5e1d02961d796
saheer17 <saheer1703@gmail.com>
2025-08-22 19:02:58 +0800

    Add java.util.Scanner import to read user input

--- src/main/java/Chintu.java
+++ src/main/java/Chintu.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chintu {
     public static void main(String[] args) {
         String logo = "  ____  _   _ ___ _   _ _____ _   _ \n"
@@ -5,10 +7,9 @@ public class Chintu {
                 + "| |    | |_| || ||  \\| | | | | | | |\n"
                 + "| |___ |  _  || || |\\  | | | | |_| |\n"
                 + " \\____||_| |_|___|_| \\_| |_|  \\___/ \n";
-        String greet_hello = "Hello! I'm Chintu!\n" +
-                "What can I do for you?\n";
-        String greet_bye = "Bye! Take care and hope to see you again soon!";
+        String greetHello = "Hello! I'm Chintu!\n" + "What can I do for you?\n";
+        String greetBye = "Bye! Take care and hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
-        System.out.println(greet_hello + greet_bye);
+        System.out.println(greetHello);
     }
 }
