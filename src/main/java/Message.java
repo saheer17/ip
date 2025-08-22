a0e0a65791a243b2c6216dd651c89ba47b0efaf4
saheer17 <saheer1703@gmail.com>
2025-08-22 19:41:08 +0800

    Create Message class to store individual texts

---
+++ src/main/java/Message.java
@@ -0,0 +1,11 @@
+public class Message {
+    private String content;
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
